dcc711bccaa2a4e2560400bf4ac349cc505dd52f
Andres Castro <49927727+andrucastro@users.noreply.github.com>
2023-01-07 18:43:25 -0700

    prep 1 finished

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
+        //Priting on the console
         Console.WriteLine("Hello World!");
+
+        //Create a conditional 
+        int number = 2;
+        if(number < 4){
+            Console.WriteLine(number);
+        }
+
+        //String interpolation
+        string test ="Esta Funcionando";
+        Console.WriteLine($"This to prove that my code {test}.");
+
+        //Create a user input comand line 
+        Console.WriteLine("Insert a color");
+        string color = Console.ReadLine();
+
+
     }
 }

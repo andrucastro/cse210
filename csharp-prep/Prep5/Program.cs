7ae2d380bf0c6cad0390f23c6fe4327684bac7ab
Andres Castro <49927727+andrucastro@users.noreply.github.com>
2023-02-03 22:58:52 -0700

    leatnig 3

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -5,41 +5,41 @@ class Program
 {
     static void Main(string[] args)
     {
-    //     DisplayWelcome();
-    //     string userName = PromptUserName();
-    //     int number = PromptUserNumber();
-    //     DisplayResult(userName,number);
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int number = PromptUserNumber();
+        DisplayResult(userName,number);
 
-    //    static void DisplayWelcome(){
-    //     Console.WriteLine("Welcome to the Program!");
-    //    };
+       static void DisplayWelcome(){
+        Console.WriteLine("Welcome to the Program!");
+       };
 
-    //    static string PromptUserName(){
-    //     Console.Write("Enter your name: ");
-    //     string name = Console.ReadLine();
-    //     return name;
-    //    }
+       static string PromptUserName(){
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+       }
 
-    //     static int PromptUserNumber(){
-    //     Console.Write("Enter your a number: ");
-    //     int number = int.Parse(Console.ReadLine());
-    //     return number;
-    //    }
+        static int PromptUserNumber(){
+        Console.Write("Enter your a number: ");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+       }
 
 
 
-    //    static int SquareNumber(int number){
+       static int SquareNumber(int number){
 
-    //     int result = number * number;
-    //     return result;
+        int result = number * number;
+        return result;
 
-    //    };
+       };
 
-    //    static void DisplayResult(string userName, int number){
+       static void DisplayResult(string userName, int number){
 
-    //     Console.WriteLine($"{userName},the square of your number is {number}");
+        Console.WriteLine($"{userName},the square of your number is {number}");
 
-    //    }
+       }
 
     TextWriter Write = new StreamWriter("mytext.txt"); 
     Write.WriteLine("This is an entry");
---
+++ prepare/Learning03/fraction.cs
@@ -0,0 +1,44 @@
+public class Fraction{
+    private int _top;
+    private int _bottom;
+
+    public Fraction(){
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int wholeNumber){
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+    public Fraction(int top, int bottom){
+         _top = top;
+        _bottom = bottom;
+    }
+    public int SetTop(int top){
+        top = _top;
+        return top;
+    }
+
+    public int GetTop(){
+        return _top;
+    }
+
+    public int SetBottom(int bottom){
+        return _bottom;
+    }
+
+    public int GetBottom(){
+        return _bottom;
+    }
+
+    public string GetFractionString(){
+        string fraction = $"{_top}/{_bottom}";
+        return fraction;
+    }
+    public double GetDecimalValue(){
+        float result = _top /_bottom;
+        return result;
+
+    }
+}
